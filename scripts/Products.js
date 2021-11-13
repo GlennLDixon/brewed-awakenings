fe62d2979321e025c20eb7e6d7de185b47464671
Glenn Dixon Jr <steve@stevenbrownlee.com>
2021-11-13 13:58:05 -0600

    created a click addEventListener with a .target property to display the price for each item clicked

--- scripts/Products.js
+++ scripts/Products.js
@@ -2,10 +2,14 @@ import { getProducts } from "./database.js"
 
 document.addEventListener("click", (clickEvent) => {
     const itemClicked = clickEvent.target
-    for (const product of products) {
-        if(product) {
-            message = window.alert(`${product.price}`)
-        }
+    if (itemClicked.id.startsWith("product")){
+        const [,productId] = itemClicked.id.split('--')
+    
+        for (const product of products) {
+            if(product.id === parseInt(productId)) {
+                window.alert(`${product.price}`)
+            }
+    }
     }
 })
 
